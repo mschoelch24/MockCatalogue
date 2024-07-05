f3d468ede04c9ba47ead7047b1c70f70a6b8f1c8
mschoelch24 <163039931+mschoelch24@users.noreply.github.com>
2024-07-05 16:42:34 +0200

    Update exe_mockcat.sh
    Adding option to iterate repeatedly over observables.py for large datasets and finally compiling the separate dataframes.

--- exe_mockcat.sh
+++ exe_mockcat.sh
@@ -4,20 +4,28 @@ input_file='Bar_ome45_short_carte.fits' #example input file, can be full path, e
 #input_file='../2Project/snapshots/PMcrs0a0.9000_wo_LMC_DM.pkl'
 
 ncores='5' # input number of cores here
+nruns='10' # input number of runs of observables.py
 
 echo "input_file: $input_file"
 echo "ncores: $ncores"
 
 start=$(date +%s)
-#
-#
+
 # running the coordinate transformations
 python3 coord_transform.py "$input_file"
-#
+
 # adding other observables such as the G magnitude and uncertainties, and the Marshall & Lallement extinction model
-python3 observables.py "$input_file" "$ncores"
-#
-#
+N=$nruns
+# looping this file for n times, include n as input so files can be labelled pt n
+for ((i=1; i<=N; i++))
+do
+        echo "Executing iteration $i of observables.py"
+        python3 observables.py "$input_file" "$ncores" "$nruns" $i
+done
+
+# compiling the separate dataframes from each iteration of observables.py and merging with the dataframe from coord_transform.py 
+python3 compile.py "$input_file"
+
 end=$(date +%s)
 
 seconds=$(echo "$end - $start" | bc)
