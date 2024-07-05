13a38fa2c9cf7c65e8eeb779b171b808d0011a4f
mschoelch24 <163039931+mschoelch24@users.noreply.github.com>
2024-07-05 16:34:56 +0200

    Update observables.py for repeat runs

--- observables.py
+++ observables.py
@@ -5,25 +5,34 @@ import sys
 def main():
     input_file = sys.argv[1]
     ncores = sys.argv[2]
+    nruns = int(sys.argv[3])
+    n = int(sys.argv[4])
 
     simname,_ = os.path.splitext(input_file)
     input_df = simname + '_coords.csv'
 
     t0 = time.time()
     df = pd.read_csv(input_df, index_col=0)
-    print("Opening simulation",simname, "with columns", list(df),"and length", len(df))
     
-    source_id = range(1, len(df) + 1) # Assigning a source id for sorting.
-    df['source_id'] = source_id
+    # set limits of sources to process in this run:
+    s_tot = len(df)
+    s_i = n * (s_tot//nruns) - (s_tot//nruns)
+    s_f = n * (s_tot//nruns)
+
+    # apply limits to the dataframe:
+    df = pd.read_csv(input_df, index_col=0)[s_i:s_f]
+    print("Processing stars", s_i, "to", s_f, "in run", n , "/", nruns)
+
+    source_id = range(s_i, s_f) # Assigning a source id for sorting.
+
+    # New data frame to save only the observables.
+    dfobs = pd.DataFrame()
+    dfobs['source_id'] = source_id
 
     t1 = time.time()
-    t_import = t1 - t0
-    print("Import time: ", '%.2f' % (t_import/60) ,"min")
-
-    
     print("****Starting extinction calculation****")
     extarray = compext_parallel(df['l'],df['b'],df['d'],df['source_id'], int(ncores))
-    df['Av_lm'] = np.array(extarray)
+    dfobs['Av_lm'] = np.array(extarray)
 
     t2 = time.time()
     t_ext = t2 - t1
@@ -31,9 +40,9 @@ def main():
 
     
     print("****Starting G mag calculation****")
-    G = magnitude(df['d'], df['Av_lm'])
-    print("G mag min, max, median:", np.min(G), np.max(G), np.nanmedian(G))
-    df['G'] = G
+    G = magnitude(np.array(df['d']), np.array(dfobs['Av_lm']))
+    #print("G mag min, max, median:", np.min(G), np.max(G), np.nanmedian(G))
+    dfobs['G'] = G
 
     t3 = time.time()
     t_Gmag = t3 - t1
@@ -41,17 +50,17 @@ def main():
 
     
     print("****Starting uncertainties calculation****")
-    plx_unc, pmra_unc, pmdec_unc = uncertainties(G)
-    df['plx_unc'] = plx_unc
-    df['pmra_unc'] = pmra_unc
-    df['pmdec_unc'] = pmdec_unc
+    plx_error, pmra_error, pmdec_error = uncertainties(G)
+    dfobs['plx_error'] = plx_error
+    dfobs['pmra_error'] = pmra_error
+    dfobs['pmdec_error'] = pmdec_error
 
-    df.to_csv(simname + '_out.csv') 
-    print("Final dataframe contains columns", list(df), "and has length", len(df))
+    dfobs.to_csv(simname + '_observ_out_pt'+ str(n) +'.csv')
+    print("Observables dataframe no.", n ,"contains columns", list(dfobs), "and has length", len(dfobs))
 
     t4 = time.time()
     t_total = t4 - t0
-    print("Total time observables:", '%.2f' % (t_total/60) ,"min")
+    print("Total time observables.py {}/{}".format(n, nruns),":", '%.2f' % (t_total/60) ,"min")
 
 if __name__ == "__main__":
     main()
